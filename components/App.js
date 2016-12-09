7cf36b804cb9df25140a3f396735ee2b52f94d9d
Alec Cunningham <aleccunningham96@gmail.com>
2016-12-08 18:11:03 -0600

    added demo code

--- components/App.js
+++ components/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 
+import Demo from './Demo.js'
+
 export default class App extends Component {
   render() {
     return (
         <div>
           <h1>PLEASE WORK</h1>
+          <Demo/>
         </div>
     );
   }
