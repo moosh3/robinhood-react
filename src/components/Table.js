ab0c19a84fba26521a7b2f8dc0340d4e37501567
Alec Cunningham <aleccunningham96@gmail.com>
2017-03-14 01:08:08 -0500

    add row and table components

--- src/components/Table.js
+++ src/components/Table.js
@@ -8,9 +8,32 @@ const propTypes = {
 class Table extends Component {
 
   render() {
-    const {} = this.props;
-
-    return (
+      var items = [];
+      for (var symbol in this.props.stocks) {
+          var stock = this.props.stocks[symbol];
+          items.push(<StockRow key={stock.symbol} stock={stock} last={this.props.last} unwatchStockHandler={this.props.unwatchStockHandler}/>);
+      }
+      return (
+          <div className="row">
+          <table className="table-hover">
+              <thead>
+                  <tr>
+                      <th>Symbol</th>
+                      <th>Open</th>
+                      <th>Last</th>
+                      <th>Change</th>
+                      <th>High</th>
+                      <th>Low</th>
+                      <th>Unwatch</th>
+                  </tr>
+              </thead>
+              <tbody>
+                  {items}
+              </tbody>
+          </table>
+          </div>
+      );
+    }
 
     )
   }
