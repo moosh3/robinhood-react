a98cd81ca05c730419dd936ff8c21e793e587374
Alec Cunningham <aleccunningham96@gmail.com>
2017-03-14 15:48:42 -0500

    change configurestore for isomorphic javascript

--- shared/store/configureStore.js
+++ shared/store/configureStore.js
@@ -2,9 +2,10 @@ import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers/rootReducer';
 import thunk from 'redux-thunk';
 
-export default function configureStore() {
+export default function configureStore(initialState) {
   const store = createStore(
       rootReducer,
+      initialState,
       applyMiddleware(thunk)
     );
 
