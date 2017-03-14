c843ca8c289107ba58eea5031fa87349a9d624fb
Alec Cunningham <aleccunningham96@gmail.com>
2017-03-14 15:21:01 -0500

    refactoring

--- shared/store/configureStore.js
+++ shared/store/configureStore.js
@@ -3,8 +3,18 @@ import rootReducer from '../reducers/rootReducer';
 import thunk from 'redux-thunk';
 
 export default function configureStore() {
-  return createStore(
-    rootReducer,
-    applyMiddleware(thunk),
-  );
+  const store = createStore(
+      rootReducer,
+      applyMiddleware(thunk)
+    );
+
+    if (module.hot) {
+      // Enable Webpack hot module replacement for reducers
+      module.hot.accept('../reducers', () => {
+        const nextRootReducer = require('../reducers').default
+        store.replaceReducer(nextRootReducer)
+      });
+    }
+
+    return store
 }
--- webpack.config.js
+++ webpack.config.js
@@ -8,23 +8,21 @@ import { WDS_PORT } from './src/shared/config'
 import { isProd } from './src/shared/util'
 
 
-var mainPath = path.resolve(__dirname, 'src', 'index.js');
+var mainPath = path.resolve(__dirname, 'client', 'index.js');
 
 const config = {
   // Entry point for bundle
   entry: [
     // The script refreshing the browser on none hot updates
-    'webpack-dev-server/client?http://localhost:8080',
-    'webpack/hot/only-dev-server',
-    'react-hot-loader/patch',
+    'webpack-hot-middleware/client',
     // Our application
     mainPath
   ],
   // Options affecting output of bundle
   output: {
-    path: path.resolve(__dirname, 'dist/js'),
+    path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: `0.0.0.0:${WDS_PORT}/dist/js/`,
+    publicPath: '/static/',
   },
   // Options affecting normal modules
   module: {
@@ -40,19 +38,14 @@ const config = {
       }
     ]
   },
-  devServer: {
-    port: WDS_PORT,
-    hot: true,
-  },
   // Third party loaders and plugins
   plugins: debug ? [] : [
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
     }),
-    new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NamedModulesPlugin(),
-    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.NoErrorsPlugin(),
   ],
 }
 
